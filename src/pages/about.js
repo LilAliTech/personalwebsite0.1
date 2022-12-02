5f61d8f249e0ed268c73fb817c40984d401faf79
LilAliTech <lilalix20@gmail.com>
2022-12-02 17:30:35 +0000

    Main

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from "react";
 import './Aboutstyle.css'
 export default function About() {
   return (
-    <section id="about">
+    <section id="about" className="aboutsec">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
@@ -24,7 +24,7 @@ export default function About() {
             <a
               href="./Projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              See My Past Work
+              See My Projects
             </a>
           </div>
         </div>
